b3ab5df393ebf04644ef6064f097524f1f4748c6
AaravBaori <e1459317@u.nus.edu>
2026-01-27 23:51:35 +0800

    Level-0

---
+++ src/main/java/Skywalker.java
@@ -0,0 +1,11 @@
+public class Skywalker {
+    public static void main(String[] args) {
+
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Skywalker");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
