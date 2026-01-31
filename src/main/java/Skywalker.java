6f91e32d0aa0f31b40cb65d62252a11b0e823ee9
AaravBaori <e1459317@u.nus.edu>
2026-01-31 15:04:56 +0800

    Level-1

--- src/main/java/Skywalker.java
+++ src/main/java/Skywalker.java
@@ -1,3 +1,6 @@
+import java.lang.*;
+import java.util.Scanner;
+
 public class Skywalker {
     public static void main(String[] args) {
 
@@ -5,6 +8,17 @@ public class Skywalker {
         System.out.println("Hello! I'm Skywalker");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
+        Scanner scanner = new Scanner(System.in);
+
+        String user_input;
+        user_input = scanner.nextLine();
+
+        while(!user_input.equalsIgnoreCase("bye")){
+            System.out.println("____________________________________________________________");
+            System.out.printf("\t%s\n",user_input);
+            System.out.println("____________________________________________________________");
+            user_input = scanner.nextLine();
+        }
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
     }
