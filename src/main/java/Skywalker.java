55a3ab1b4d939212f8a344ff33f7155bd6c91713
AaravBaori <e1459317@u.nus.edu>
2026-01-31 15:15:56 +0800

    Level-2

--- src/main/java/Skywalker.java
+++ src/main/java/Skywalker.java
@@ -12,12 +12,24 @@ public class Skywalker {
 
         String user_input;
         user_input = scanner.nextLine();
+        int index = 0;
+        String[] user_inputs = new String[100];
 
         while(!user_input.equalsIgnoreCase("bye")){
             System.out.println("____________________________________________________________");
-            System.out.printf("\t%s\n",user_input);
-            System.out.println("____________________________________________________________");
-            user_input = scanner.nextLine();
+            if(user_input.equalsIgnoreCase("list")){
+                int num = 1;
+                for(int i = 0; i< index; i++){
+                    System.out.printf("\t%d. %s\n",num, user_inputs[i]);
+                    num++;
+                }
+            }
+            else {
+                user_inputs[index++] = user_input;
+                System.out.printf("\tadded: %s\n", user_input);
+            }
+                System.out.println("____________________________________________________________");
+                user_input = scanner.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
