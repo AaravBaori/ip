7f42b49fdfce1b75f8f9bf35a9bb452b8dd7a146
AaravBaori <e1459317@u.nus.edu>
2026-02-04 13:10:41 +0800

    Level-4

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -10,7 +10,7 @@ public class Event extends Task{
 
     @Override
     public String toString(){
-        return "[E]" + super.toString() + " (from: " + this.from + " to: " + this.to;
+        return "[E]" + super.toString() + " (from: " + this.from + " to: " + this.to + ")";
     }
 
     public void setFrom(String from){
